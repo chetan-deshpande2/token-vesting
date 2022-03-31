8292b4df910d3d394c97e71d2d0f67b8138ff4e1
Chetan Deshpande <cipher@pop-os.localdomain>
2022-03-31 18:17:48 +0530

    boilerplate code and folder structure

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('dotenv').config();
+
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.2'
+      }
+    ]
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_RPC_URL,
+      accounts: [
+        process.env.RINKEBY_PRIVATE_KEY_ACCOUNT1,
+        process.env.RINKEBY_PRIVATE_KEY_ACCOUNT3
+      ],
+      gasPrice: 3000000000
+    }
+  },
+
+  etherscan: {
+    apiKey: process.env.API_KEY
+  }
+};
