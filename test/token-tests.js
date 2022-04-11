2020683ab2e7de423be36fd91612aa33f18d7a6c
Chetan Deshpande <cipher@pop-os.localdomain>
2022-04-11 13:52:43 +0530

    all transaction related tests added

--- test/token-tests.js
+++ test/token-tests.js
@@ -32,5 +32,33 @@ describe('Token contract', function () {
     });
   });
 
+  describe('Transactions', function () {
+    it('Should transfer tokens between accounts', async function () {
+      // Transfer 50 tokens from owner to addr1
+      await token.transfer(addr1.address, 50);
+      const addr1Balance = await token.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(50);
 
+      // Transfer 50 tokens from addr1 to addr2
+      // We use .connect(signer) to send a transaction from another account
+      await token.connect(addr1).transfer(addr2.address, 50);
+      const addr2Balance = await token.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(50);
+    });
+
+    it('Should fail if sender doesn’t have enough tokens', async function () {
+      const initialOwnerBalance = await token.balanceOf(owner.address);
+
+      // Try to send 1 token from addr1 (0 tokens) to owner (1000000 tokens).
+      // `require` will evaluate false and revert the transaction.
+      await expect(
+        token.connect(addr1).transfer(owner.address, 1)
+      ).to.be.revertedWith('Not enough tokens');
+
+      // Owner balance shouldn't have changed.
+      expect(await token.balanceOf(owner.address)).to.equal(
+        initialOwnerBalance
+      );
+    });
+  });
 });
