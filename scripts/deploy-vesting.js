ecc24e7a7e63101fabfe9129e9966fd4be7c736b
chetan-deshpande2 <chetan.deshpande@protonmail.com>
2022-04-11 17:46:47 +0530

    vesting contract deployment script

--- scripts/deploy-vesting.js
+++ scripts/deploy-vesting.js
@@ -1,14 +1,14 @@
 const hre = require('hardhat');
 
 async function main() {
-  let _token = '';
+  let _token = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
   const VestingContract = await hre.ethers.getContractFactory('Vesting');
   const vesting = await VestingContract.deploy(_token);
 
   await vesting.deployed();
 
-  console.log('Vesting contract deployed Address:', token.address);
+  console.log('Vesting contract deployed Address:', vesting.address);
 }
 
 main()
