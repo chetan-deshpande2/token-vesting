610c23687cccfb4df9179f76e14af9bd7a4b4852
Chetan Deshpande <cipher@pop-os.localdomain>
2022-04-11 13:53:57 +0530

    renamed name of contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token  is ERC20 {
   
-  constructor() ERC20("Vesting Token  ","VST") {
+  constructor() ERC20("Vesting Token","VST") {
       _mint(msg.sender, 1000000 *10**18);
   }
 
