628b9cf182dddc476eb6c67488253a571ffd0de8
Chetan <cipher@pop-os.localdomain>
2022-04-15 10:25:58 +0530

    enabled optimizations

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-etherscan');
-require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
-task('accounts', 'Prints the list of accounts', async (hre) => {
+task("accounts", "Prints the list of accounts", async (hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -14,30 +14,36 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: '0.8.2'
-      }
-    ]
+        version: "0.8.2",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
   networks: {
     rinkeby: {
       url: process.env.RINKEBY_RPC_URL,
       accounts: [
         process.env.RINKEBY_PRIVATE_KEY_ACCOUNT1,
-        process.env.RINKEBY_PRIVATE_KEY_ACCOUNT3
+        process.env.RINKEBY_PRIVATE_KEY_ACCOUNT3,
       ],
-      gasPrice: 3000000000
+      gasPrice: 3000000000,
     },
     goerli: {
       url: process.env.RINKEBY_RPC_URL,
       accounts: [
         process.env.RINKEBY_PRIVATE_KEY_ACCOUNT1,
-        process.env.RINKEBY_PRIVATE_KEY_ACCOUNT3
+        process.env.RINKEBY_PRIVATE_KEY_ACCOUNT3,
       ],
-      gasPrice: 3000000000
-    }
+      gasPrice: 3000000000,
+    },
   },
 
   etherscan: {
-    apiKey: process.env.API_KEY
-  }
+    apiKey: process.env.API_KEY,
+  },
 };
